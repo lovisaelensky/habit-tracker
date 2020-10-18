93d9195fb74bddeafe00f043865f64baaa8a6826
Anna Roos <anna.flytstrom@gmail.com>
2020-10-18 07:43:54 +0200

    added js
    I made a js file with oop that can create list items, publish them and save them to the localStorage. You can also check them and when three are checked the background changes.

---
+++ classes.js
@@ -0,0 +1,93 @@
+class CleaningItem {
+    constructor(task, id) {
+        this.task = task;
+        this.id = id;
+        this.check = false;
+        this.active = true;
+    }
+}
+
+
+class ListOfTasks {
+    list;
+    id = 0;
+    constructor() {
+        let data = localStorage.getItem('taskList');
+        if(data){
+            this.list = JSON.parse(data);
+            this.id = this.list.length;
+            for(const item of this.list) {
+                this.publish(item.task, item.id, item.check);
+            }
+        } else {
+            this.list = [];
+        }
+
+        this.create();
+        this.check();
+        this.changeUI();
+    }
+
+    publish = (task, id, check) => {
+        let checked = check ? 'mark' : 'unmark';
+        const DOMitem = `<li class="item">
+                        <button class="check-btn ${checked}" id="${id}"></button>
+                        <p class="cleaning-item">${task}</p>
+                        </li>`;
+        const DOMlist = document.querySelector('ul');
+        const position = 'afterbegin';
+        DOMlist.insertAdjacentHTML(position, DOMitem);
+    }
+
+    create = () => {
+        const addBtn = document.getElementById('add-btn');
+        addBtn.addEventListener('click', () => {
+            let taskInput = document.getElementById('input');
+            if(taskInput.value === ""){ return; }
+            const newItem = new CleaningItem(taskInput.value, this.id);
+            this.list.push(newItem);
+            this.publish(newItem.task, newItem.id);
+            localStorage.setItem('taskList', JSON.stringify(this.list));
+            this.id++;
+            taskInput.value = "";
+        })
+        
+    }
+
+    check = () => {
+        const list = document.querySelector('ul');
+        list.addEventListener('click', (event) => {
+            if(event.target.nodeName === 'BUTTON'){
+                let checkBtn = event.target;
+                checkBtn.classList.toggle('mark');
+                checkBtn.classList.toggle('unmark');
+                this.list[checkBtn.id].check = this.list[checkBtn.id].check ? false : true;
+                console.log(this.list[checkBtn.id].check);
+                localStorage.setItem('taskList', JSON.stringify(this.list));
+                this.changeUI();
+            }
+
+        })
+        
+    }
+
+    changeUI = () => {
+        let checkedTasks = 0;
+        for(const item of this.list) {
+            if(item.check) {
+                checkedTasks++;
+            }
+        }
+        if(checkedTasks > 2) {
+            document.getElementById('wrapper').classList.add('clean');
+        } else {
+            document.getElementById('wrapper').classList.remove('clean');
+        }
+    }
+}
+
+const newList = new ListOfTasks();
+
+
+
+        
