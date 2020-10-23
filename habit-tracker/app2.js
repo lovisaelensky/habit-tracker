3a14ba52323abb757db823b1c478846bf947d0a2
lovisaelensky <70098425+lovisaelensky@users.noreply.github.com>
2020-10-23 10:52:06 +0200

    latest updates

--- habit-tracker/app2.js
+++ habit-tracker/app2.js
@@ -4,15 +4,23 @@
 //Mockup data i local storage
 
 const completedTasks = [{ '20-10-21': [3, 2, 1], '20-10-22': [4, 5, 6] }];
-const easyArray = [1, 4, 7];
+const easyArray = [
+  { date: '20-10-22', completed: 5 },
+  { date: '20-10-21', completed: 0 },
+  { date: '20-10-20', completed: 7 },
+];
 
-function addTracking() {
+function addTracking(item) {
   const newLi = document.createElement('li');
   const ul = document.querySelector('.tracker-list');
-  newLi.innerText = 'datum';
+  //newLi.innerText = item.date;
   ul.appendChild(newLi);
 
-  for (let i = 0; i < easyArray.length; i++) {
+  const newP = document.createElement('p');
+  newP.innerText = item.date;
+  newLi.appendChild(newP);
+
+  for (let i = 0; i < item.completed; i++) {
     const newSpan = document.createElement('span');
     newSpan.innerHTML = '&#9673';
     newSpan.classList.add('color');
@@ -20,4 +28,6 @@ function addTracking() {
   }
 }
 
-addTracking();
+for (const item of easyArray) {
+  addTracking(item);
+}
